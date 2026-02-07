c3b5fe806cac3a8d1a12023cb3009ec29e6e0ae8
황인복 <dlsqhrdl1212@gmail.com>
2026-02-07 14:22:46 +0900

    테스트 API 제거

--- src/route/test.route.ts
+++ src/route/test.route.ts
@@ -1,25 +1,6 @@
 import { Router } from "express";
-import {
-  testAccessToken,
-  testFileUpload,
-  testLS,
-  testRedis,
-  testResponse,
-} from "../controller/test.controller";
-import { verifyAccessTokenMiddleware } from "../middleware/auth.middleware";
-import { uploadProfileImageMiddleware } from "../middleware/multer.middleware";
 
 const testRouter = Router();
 
-// GET api/test/important
-testRouter.get("/important", verifyAccessTokenMiddleware, testAccessToken);
-// GET api/test/response
-testRouter.get("/response", testResponse);
-// POST api/test/upload
-testRouter.post("/upload", uploadProfileImageMiddleware, testFileUpload);
-// POST api/test/redis
-testRouter.post("/redis", testRedis);
-// GET api/test/ls
-testRouter.get("/ls", testLS);
 
 export default testRouter;
